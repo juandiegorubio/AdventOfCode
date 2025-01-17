bfcd1a7e46f12c772fbea317b4d3ba0d1a2268ef
juandiegorubio <rubio.juandiego.cs@gmail.com>
2025-01-18 00:23:31 +0100

    feat: :construction: Starting Day 2 (part 1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,11 @@
-#include <bits/stdc++.h>
-#include "../include/DataStructures.h"
-using namespace std;
-using namespace std::chrono;
+#include "days.hpp"
 
 
 
-
-void main_with_linked_list() {
-    LinkedList list1, list2;
-    int n1, n2;
-    auto start = high_resolution_clock::now();
-    while (cin >> n1 >> n2) {
-        list1.insert_sort(n1);
-        list2.insert_sort(n2);
-    }
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << duration.count() << endl;
-}
-
-void main_with_vector() {
-    vector<int> l1, l2;
-    int n1, n2;
-    auto start = high_resolution_clock::now();
-    while (cin >> n1 >> n2) {
-        l1.push_back(n1);
-        l2.push_back(n2);
-    }
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    cout << duration.count() << endl;
-}
 
 int main() {
-    main_with_linked_list();
-    cout << "THIS WORKS" << endl;
+    string report_str;
+    while(getline(cin, report_str)) {
+        
+    }
 }
