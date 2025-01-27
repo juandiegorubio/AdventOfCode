e7b7c687197ff7020fe380cc1c8906acc4754873
juandiegorubio <rubio.juandiego.cs@gmail.com>
2025-01-27 21:06:31 +0100

    feat: :recycle: Redo Day 2

--- src/days.hpp
+++ src/days.hpp
@@ -12,8 +12,6 @@ extern void day1_part2();
 
 extern bool is_report_safe(string s);
 extern void day2_part1();
-extern bool is_report_safe_part2(vector<int> report);
-extern void day2_part2();
 // extern void day2_part2();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,83 @@
 #include "days.hpp"
+#include <iostream>
+using namespace std;
+
+
+
+void print_vector(vector<int> report) {
+    for(int level : report) cout << level << ' ';
+    cout << endl;
+}
+
+
+bool is_report_safe(vector<int> report) {
+    
+    int diff, level, prev_level = report[0];
+    bool is_order_increasing, order_set = false;
+
+    for(long unsigned int i=1; i < report.size(); ++i) {
+        level = report[i];
+        diff = level - prev_level;
+        
+        if( abs(diff) < 1 || abs(diff) > 3 ) return false;
+        if( order_set && (is_order_increasing != (diff > 0)) ) return false;
+        if( !order_set ) {
+            is_order_increasing = (diff > 0);
+            order_set = true;
+        }
+        
+        prev_level = level;
+    }
+    return true;
+}
+
+
+bool is_report_safe_tolerable(vector<int> report) {
+    
+    int diff, level, prev_level = report[0];
+    bool is_order_increasing, order_set = false;
+
+    for(long unsigned int i=1; i < report.size(); ++i) {
+        level = report[i];
+        diff = level - prev_level;
+
+        
+    }
+}
+
+
+
+void day2_part1() {
+    string input;
+    vector<int> report;
+    int result = 0;
+
+    while(getline(cin, input)) {
+        stringstream report_str(input);
+        for(int level=0; report_str >> level; ) report.push_back(level);
+        if( is_report_safe(report) )  result++;
+        report.clear();
+    }
+
+    cout << "Reports Safe: " << result << endl;
+}
+
+void day2_part2() {
+    string input;
+    vector<int> report;
+    int result = 0;
+
+    while(getline(cin, input)) {
+        stringstream report_str(input);
+        for(int level=0; report_str >> level; ) report.push_back(level);
+        if( is_report_safe_tolerable(report) )  result++;
+        report.clear();
+    }
+
+    cout << "Reports Safe: " << result << endl;
+}
 
 
 int main() {
-    day2_part2();
+    day2_part1();
 }
