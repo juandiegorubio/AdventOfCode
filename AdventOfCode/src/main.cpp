736d764ac06175355597d1acdbc12087e7e93af8
juandiegorubio <rubio.juandiego.cs@gmail.com>
2025-02-12 14:39:50 +0100

    feat: Upload changes

--- AdventOfCode/src/main.cpp
+++ AdventOfCode/src/main.cpp
@@ -2,8 +2,21 @@
 #include <iostream>
 using namespace std;
 
-
-
-
 int main() {
+    string mem;
+    cin >> mem;
+
+    int mult_start = mem.find("mult(")+1;
+
+    while(mult_start != mem.end()) {
+        // Get X
+        int comma_pos = mem.find(',', mult_start);
+        if(comma_pos - mult_start < 1 || comma_pos - mult_start > 3) continue;
+        int X = mem.substr()
+ 
+        int mult_end = mem.find(')', mult_start);
+        if(mult_end - comma_pos < 1 || mult_end - comma_pos > 3) continue;
+
+    }    
+
 }
